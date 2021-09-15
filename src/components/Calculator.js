6465456522fe2fc0271e12c78ba084b2eec90b5c
Hamza <hamzaoutdoors@gmail.com>
2021-09-15 23:44:39 +0100

    Add events to display operation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,45 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState((prev) => calculate(prev, e.target.name));
   }
 
   render() {
+    const { total, next, operation } = this.state;
+    const buttons = [['AC', '+/-', '%', '÷'], ['7', '8', '9', 'x'], ['4', '5', '6', '-'], ['1', '2', '3', '+']].map((row) => (
+      <div className="row" key={row[0]}>
+        <button type="button" className="col" id={row[0]} name={row[0]} onClick={this.handleClick}>{row[0]}</button>
+        <button type="button" className="col" id={row[1]} name={row[1]} onClick={this.handleClick}>{row[1]}</button>
+        <button type="button" className="col" id={row[2]} name={row[2]} onClick={this.handleClick}>{row[2]}</button>
+        <button type="button" className="col operature" id={row[3]} name={row[3]} onClick={this.handleClick}>{row[3]}</button>
+      </div>
+    ));
     return (
       <>
         <div className="calc-container">
           <form id="form">
-            <input type="text" id="result" value="0" disabled />
+            <input type="text" id="result" value={(total || 0) + (operation || '') + (next || '')} disabled />
           </form>
           <div className="container">
+            {buttons}
             <div className="row">
-              <button type="button" className="col">AC</button>
-              <button type="button" className="col">+/-</button>
-              <button type="button" className="col">%</button>
-              <button type="button" className="col operature">÷</button>
-            </div>
-            <div className="row">
-              <button type="button" className="col">7</button>
-              <button type="button" className="col">8</button>
-              <button type="button" className="col">9</button>
-              <button type="button" className="col operature">x</button>
-            </div>
-            <div className="row">
-              <button type="button" className="col">4</button>
-              <button type="button" className="col">5</button>
-              <button type="button" className="col">6</button>
-              <button type="button" className="col operature">-</button>
-            </div>
-            <div className="row">
-              <button type="button" className="col">1</button>
-              <button type="button" className="col">2</button>
-              <button type="button" className="col">3</button>
-              <button type="button" className="col operature">+</button>
-            </div>
-            <div className="row">
-              <button type="button" className="col-6">0</button>
-              <button type="button" className="col-3">.</button>
-              <button type="button" className="col-3 operature">=</button>
+              <button type="button" className="col-6" name="0" onClick={this.handleClick}>0</button>
+              <button type="button" className="col-3" name="." onClick={this.handleClick}>.</button>
+              <button type="button" className="col-3 operature" name="=" onClick={this.handleClick}>=</button>
             </div>
           </div>
         </div>
