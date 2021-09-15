f0f3add957f51204f3b097f27c3e7d6ee18a472b
Hamza <hamzaoutdoors@gmail.com>
2021-09-15 01:09:01 +0100

    Refactor App component from function to class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-/* eslint-disable react/no-unescaped-entities */
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 import logo from './media/logo.png';
 
-function App() {
-  return (
-    <div className="app">
-      <img
-        src={logo}
-        alt="Logo"
-        className="logo"
-      />
-      <div className="app-container">
-        <h2 className="app-header">Let's do some math!</h2>
-        <Calculator />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <img
+          src={logo}
+          alt="Logo"
+          className="logo"
+        />
+        <div className="app-container">
+          <h2 className="app-header">Lets do some math!</h2>
+          <Calculator />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
