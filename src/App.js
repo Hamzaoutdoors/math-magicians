92fcc25fb74b640a7f84cfe9dab11a4392dc5657
Hamza <hamzaoutdoors@gmail.com>
2021-09-16 19:20:54 +0100

    Refactor App component to function

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
-/* eslint-disable no-console */
-import React, { Component } from 'react';
-import Calculator from './components/Calculator';
+import React from 'react';
 import logo from './media/logo.png';
+import Calculator from './components/Calculator';
 import Clock from './components/Clock';
-import calculate from './logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
+const App = () => (
+  <>
+    <div className="app">
+      <img
+        src={logo}
+        alt="Logo"
+        className="logo"
+      />
+      <div className="app-container">
+        <Clock />
+        <h2 className="app-header">Lets do some math!</h2>
+        <Calculator />
+      </div>
+    </div>
+  </>
 
-    this.updateState = this.updateState.bind(this);
-  }
-
-  updateState(buttonName) {
-    this.setState((prev) => calculate(prev, buttonName));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    const calculateObj = { total, next, operation };
-    return (
-      <>
-        <div className="app">
-          <img
-            src={logo}
-            alt="Logo"
-            className="logo"
-          />
-          <div className="app-container">
-            <Clock />
-            <h2 className="app-header">Lets do some math!</h2>
-            <Calculator calculateObj={calculateObj} updateState={this.updateState} />
-          </div>
-        </div>
-      </>
-
-    );
-  }
-}
+);
 
 export default App;
