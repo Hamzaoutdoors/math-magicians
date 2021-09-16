1a19a91ab3611d3c291add5f9fb7ddad50cf01ee
Hamza <hamzaoutdoors@gmail.com>
2021-09-16 17:37:54 +0100

    add propTypes to calculator object

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/forbid-prop-types */
-/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
