6dcba362d039af731b17bc389f9abbb6d976da7c
Hamza <hamzaoutdoors@gmail.com>
2021-09-19 18:35:23 +0100

    Add navbar with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import {
+  Navbar, Container, Nav,
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './media/logo.png';
 import Calculator from './components/Calculator';
 import Clock from './components/Clock';
 
 const App = () => (
   <>
+    {/* Navbar with bootstrap */}
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            alt="Logo"
+            className="logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Calculator</Nav.Link>
+            <Nav.Link href="#link">Quote</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    {/* Main section */}
     <div className="app">
-      <img
-        src={logo}
-        alt="Logo"
-        className="logo"
-      />
       <div className="app-container">
         <Clock />
         <h2 className="app-header">Lets do some math!</h2>
