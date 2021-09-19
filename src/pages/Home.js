6808f08f587b934937b0072f3f677976095d77a2
Hamza <hamzaoutdoors@gmail.com>
2021-09-20 00:05:52 +0100

    Add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
+  <div className="home-page">
     <h1>Hamza in home</h1>
   </div>
 );
--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import quoteImage from '../media/hamzaQuote.png';
 
 const Quote = () => (
-  <div>
-    <h2>Hamza</h2>
+  <div className="quote">
+    <h2>Motivational math quotes</h2>
+    <p className="quote-body">
+      If people do not believe that mathematics is simple,
+      it is only because they do not realize how complicated life is.
+    </p>
+    <strong> ~ John von Neumann, American-hungarian mathematician</strong>
+    <img
+      src={quoteImage}
+      alt="math"
+      className="math"
+    />
   </div>
 );
 
